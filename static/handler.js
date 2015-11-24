b4a1ac95f0c523ddf3eea88d355ed381b77125af
chiefenne <andreas.ennemoser@gmail.com>
2015-11-24 01:31:22 +0100

    Refactoring and improving response

--- static/handler.js
+++ static/handler.js
@@ -3,31 +3,76 @@
 //
 
 function onDoorClicked(obj, id) {
-  var xhttp;
+  var request;
+  var textid;
+  request = new XMLHttpRequest();
+  textid = 'response_text'
+
+  // disable clicking until relay action finished
+  document.getElementById('pedestrian').style.pointerEvents = 'none';
+  document.getElementById('gate').style.pointerEvents = 'none';
   
-  xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-  if (xhttp.readyState == 4 && xhttp.status == 200) {
-    document.getElementById('response_text').innerHTML = xhttp.responseText;
-    }
+  if (id == 'pedestrian') {
+      message = 'Side gate operating'
+  } else { 
+      message = 'Car gate operating'
   }
-  xhttp.open('GET', '/door?id='+id, true);
-  xhttp.send();
+
+  document.getElementById(textid).innerHTML = message;
+  setTimeout(function() {
+      clearResponseText(textid);
+  }, 2000);
+ 
+  request.onreadystatechange = function() {
+
+    // enable clicking after relay action finished
+    document.getElementById('pedestrian').style.pointerEvents = 'auto';
+    document.getElementById('gate').style.pointerEvents = 'auto';
+  }
+  }
+ 
+  request.open('GET', '/door?id='+id, true);
+  request.send();
+
 }
 
 function onCameraClicked(obj, id) {
-  var xhttp;
+  var image;
+  var request;
+  var textid;
+  request = new XMLHttpRequest();
+  textid = 'cam_response_text'
   var cam_pos = { 'l-cam': 'left', 'm-cam': 'middle', 'r-cam': 'right'};
-  
-  xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-  if (xhttp.readyState == 4 && xhttp.status == 200) {
-    document.getElementById('cam_response_text').innerHTML = xhttp.responseText;
-    // add time to request to act as cachebreaker
+
+  if (id == 'l-cam') {
+    message = 'Photo has been taken'
+  } else { 
+    message = 'Photo has been taken'
+  }
+
+  document.getElementById(textid).innerHTML = message;
+  setTimeout(function() {
+      clearResponseText(textid);
+  }, 2000);
+ 
+  request.onreadystatechange = function() {
+  if (request.readyState == 4 && request.status == 200) {
+
+    if (request.responseText == 'Default image') {
+      image = 'placeholder.png'
+    } else { 
+      image = 'camera.png'
+    }
+    // add current time to request to act as cachebreaker
     // i.e. photo will be loaded again instead of being cached
-    document.getElementById(cam_pos[id]).src='my_image.png?' + new Date().getTime();
+    document.getElementById(cam_pos[id]).src=image + '?' + new Date().getTime();
     }
   }
-  xhttp.open('GET', '/cam?id='+id, true);
-  xhttp.send();
+
+  request.open('GET', '/cam?id='+id, true);
+  request.send();
+}
+
+function clearResponseText(id) {
+  document.getElementById(id).innerHTML = ' '
 }
